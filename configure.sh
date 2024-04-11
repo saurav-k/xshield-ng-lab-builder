04042a28af0449fcc2557286ffd1509ad48b0c75
vr-ct <venky.raju@colortokens.com>
2024-04-11 11:20:46 -0700

    Added aws config parameters for region and auth profile

--- configure.sh
+++ configure.sh
@@ -11,16 +11,18 @@
 # Venky Raju
 
 rm -f terraform/terraform.tfvars
+rm -f terraform/provider.tf
 rm -f xshield/config.json
 
-domain=$(grep domain config.txt | cut -d "=" -f2)
-tenantId=$(grep tenantId config.txt | cut -d "=" -f2)
-userId=$(grep userId config.txt | cut -d "=" -f2)
-fingerprint=$(grep fingerprint config.txt | cut -d "=" -f2)
-privateKey=$(grep privateKey config.txt | cut -d "=" -f2)
+
+domain=$(grep "^domain=" config.txt | cut -d "=" -f2)
+tenantId=$(grep "^tenantId=" config.txt | cut -d "=" -f2)
+userId=$(grep "^userId=" config.txt | cut -d "=" -f2)
+fingerprint=$(grep "^fingerprint=" config.txt | cut -d "=" -f2)
+privateKey=$(grep "^privateKey=" config.txt | cut -d "=" -f2)
 
 if [ -z "$domain" ] || [ -z "$tenantId" ] || [ -z "$userId" ] || [ -z "$fingerprint" ] || [ -z "$privateKey" ]; then
-    echo "One or more parameters are undefined"    
+    echo "One or more of (domain, tenantId, userId, fingerprint, privateKey) are undefined"    
     exit 1
 fi
 
@@ -38,14 +40,14 @@ cat > xshield/config.json <<EOC
 }
 EOC
 
-owner=$(grep owner config.txt | cut -d "=" -f2)
+owner=$(grep "^owner" config.txt | cut -d "=" -f2)
 deployment_key=$(cd xshield && python3 -c "import agents;print(agents.get_deployment_key())")
 gk_pkg=$(cd xshield && python3 -c "import agents;print(agents.get_agent_installer('CT_GATEWAY', 'debian', 'x86_64'))")
 deb_pkg=$(cd xshield && python3 -c "import agents;print(agents.get_agent_installer('CT_AGENT', 'debian', 'x86_64'))")
 win_pkg=$(cd xshield && python3 -c "import agents;print(agents.get_agent_installer('CT_AGENT', 'windows', 'x86_64'))")
 
 if [ -z "$owner" ] || [ -z "$deployment_key" ] || [ -z "$gk_pkg" ] || [ -z "$deb_pkg" ] || [ -z "$win_pkg" ]; then
-    echo "One or more parameters are undefined"       
+    echo "One or more of (owner, deployment_key, gk_pkg, deb_pkg, win_pkg ) are undefined"       
     exit 1
 fi
 
@@ -58,3 +60,20 @@ xs_gatekeeper_pkg_url = "${gk_pkg}"
 xs_agent_debian_pkg_url = "${deb_pkg}"
 xs_agent_windows_pkg_url = "${win_pkg}"
 EOT
+
+
+profile=$(grep "^aws-profile" config.txt | cut -d "=" -f2)
+region=$(grep "^aws-region" config.txt | cut -d "=" -f2)
+
+if [ -z "$profile" ] || [ -z "$region" ]; then
+    echo "One or more of aws-profile, aws-region are undefined"       
+    exit 1
+fi
+
+echo "Writing terraform/provider.tf"
+cat > terraform/provider.tf <<EOP
+provider "aws" {
+  profile = "$profile"
+  region  = "$region"
+}
+EOP
