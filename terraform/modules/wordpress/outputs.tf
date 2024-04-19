14f8031ca79eb661947fd9694b18d3b2aea57a5c
vr-ct <venky.raju@colortokens.com>
2024-04-18 19:40:32 -0700

    Fixed description

--- terraform/modules/wordpress/outputs.tf
+++ terraform/modules/wordpress/outputs.tf
@@ -1,4 +1,4 @@
 output "wordpress_public_ip" {
-    description = "Public IP address of the Wordpress site"
+    description = "Public IP address of the Wordpress web server"
     value = aws_instance.webapp.public_ip
 }
